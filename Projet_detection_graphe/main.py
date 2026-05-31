aa2a18049d4f96ed43e70cac940ee95f0d228fdd
Lewis-Benjamin <167799849+Lewis-Benjamin@users.noreply.github.com>
2026-05-31 20:54:01 +0300

    Update main.py

--- Projet_detection_graphe/main.py
+++ Projet_detection_graphe/main.py
@@ -1,21 +1,10 @@
 #coding: utf-8
 
-import base_de_donnees
-import graphe
-import detection_des_dealers as ddd
+import creation_graph
+import classification
+import new_base_de_donnees
 
-# Destruction de la base de donnees dans neo4j
-base_de_donnees.clear_database()
-
-# Creation base de donnees
-nb_membres = input("Entrez le nombre de population dans la communaute: ")
-societe = base_de_donnees.generate_communite(nb_membres)
-relations = base_de_donnees.generate_friendship(societe)
-societe = base_de_donnees.simulation_transaction(societe, relations)
-base_de_donnees.creation_base_de_donnees(societe, relations)
-
-# Detection des dealers, consommateurs et des suspects
-dealers, maybe_dealers, consommateurs = ddd.parcour_graph()
-
-# Affichage sous networkx et matplotlib.pyplot
-graphe.generation_du_graphe_sur_matplotib()
+graph = creation_graph.generation_graph()
+liste_sommets, graph = classification.generation_graphe_classifie(graph)
+new_base_de_donnees.clear_database()
+new_base_de_donnees.creation_base_de_donnees(liste_sommets, graph)
