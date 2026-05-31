0503e3887b0f440cf601a1ade7e1f71cd2f71199
Lewis-Benjamin <167799849+Lewis-Benjamin@users.noreply.github.com>
2026-05-31 20:55:44 +0300

    Update dealer.py

--- Projet_detection_graphe/dealer.py
+++ Projet_detection_graphe/dealer.py
@@ -8,35 +8,37 @@ class Dealer(Consommateur, Etudiant):
     nb_dealer = 0
     def __init__(self):
         super().__init__()
-        self.clients = []
+        self.clients = list()
         self.nb_transaction = 0
         self.nb_client = 0
         self.nb_consomme = self.consommation()
 
         #Augmenter le nombre de dealer
         Dealer.nb_dealer += 1
+        #Etudiant.nombre_etudiant -= 1
         self.dealer_id = Dealer.nb_dealer
     
-    def transaction(self, etu: Etudiant)-> ec:
+    def transaction(self, etu: Etudiant):
         obj = ec()
-
-        #copies des attributs de l'eleve a corrompre dans obj
-        obj.niveau = etu.niveau
-        obj.id = etu.id
-        obj.sexe = etu.sexe
-        obj.compte_banc = etu.compte_banc
-        obj.num_tel = etu.num_tel
-
-        #augmentation du nombre de transaction et de la solde du dealer
-        self.nb_transaction += obj.nb_transaction
-        self.solde += obj.nb_transaction*71
-
-        #augmentation du nombre de client et enregistrement des clients
-        self.nb_client += 1
-        self.clients.append(obj)
-
-        return obj
-    
+        try:
+            etu.taux_d_addiction
+        except AttributeError:
+            try:
+                etu.dealers
+            except AttributeError:
+                obj.niveau = etu.niveau
+                obj.id = etu.id
+                obj.sexe = etu.sexe
+                obj.compte_banc = etu.compte_banc
+                obj.num_tel = etu.num_tel
+                self.nb_transaction += obj.nb_transaction
+                self.solde += obj.nb_transaction * 71
+                self.nb_client += 1
+                self.clients.append(obj)
+                Etudiant.nombre_etudiant -= 1
+                return obj  
+        return etu 
+        
     def consommation(self):      #le total de depense mensuel en euro
         self.taux_d_addiction = random.randint(0, 3)
         if self.taux_d_addiction == 1:
