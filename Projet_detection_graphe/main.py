e045fc250e64b4866de950ec6c42a1160790b085
Lewis-Benjamin <167799849+Lewis-Benjamin@users.noreply.github.com>
2026-06-01 16:49:39 +0300

    Update main.py

--- Projet_detection_graphe/main.py
+++ Projet_detection_graphe/main.py
@@ -4,7 +4,7 @@ import creation_graph
 import classification
 import new_base_de_donnees
 
-graph = creation_graph.generation_graph()
-liste_sommets, graph = classification.generation_graphe_classifie(graph)
-new_base_de_donnees.clear_database()
-new_base_de_donnees.creation_base_de_donnees(liste_sommets, graph)
+base_de_donnees.clear_database()
+graphe = creation_graph.generation_graph()
+historique = base_de_donnees.creation_base_de_donnees(graphe)
+base_de_donnees.creation_liens(graphe, historique)
